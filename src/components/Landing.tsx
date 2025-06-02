91ac62946bd0bf3724a528657941624589bbe328
asgold8 <asgold8@gmail.com>
2025-06-02 14:30:25 -0700

    added styling to the landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,12 +5,22 @@ const Landing = () => {
     <div>
       <h2>Welcome to the page!</h2>
       <div>Feel free to navigate the links above or below.</div>
-      <ul>
+      <ul className="mt-6 flex flex-col items-start space-y-2">
         <li>
-          <Link to="/About">About</Link>
+          <Link
+            to="/asgold8-portfolio/About"
+            className="text-blue-600 hover:text-blue-800 font-medium transition-colors"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/Projects">Projects</Link>
+          <Link
+            to="/asgold8-portfolio/Projects"
+            className="text-blue-600 hover:text-blue-800 font-medium transition-colors"
+          >
+            Projects
+          </Link>
         </li>
       </ul>
     </div>
